5eab6ad1fd698b292a03a642c75ba422f92c7807
Ngima Sherpa <sherpa.ngima@hotmail.com>
2017-11-14 15:01:33 +0545

    initial commit

---
+++ app/src/main/java/me/ngima/lambda/MainActivity.java
@@ -0,0 +1,10 @@
+package me.ngima.lambda;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mangi on 14/11/2017.
+ */
+
+public class MainActivity extends AppCompatActivity{
+}
